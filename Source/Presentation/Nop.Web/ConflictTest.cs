23c226d67e9cfeb65f7a92544e2a7170003d7815
seydasan <seydasan717@gmail.com>
2020-06-12 00:49:07 +0300

    Conflict testlerimiz

---
+++ Source/Presentation/Nop.Web/ConflictTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nop.Web
+{
+    public class ConflictTest
+    {
+        public void deneme ()
+        {
+            int a = 5;
+            int b = 4;
+            int sum = a + b;
+        }
+
+    }
+}
